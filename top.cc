53904946e13834a366ff75f593d2b549b1f94c4a
bjkim <bjk.bears@gmail.com>
2026-01-25 23:45:37 +0900

    real_final

--- top.cc
+++ top.cc
@@ -30,11 +30,6 @@ int main() {
                 loader.load_IA(conv_layer.IA, c);
                 loader.Load_FW(conv_layer.FW, k, k + Scnn::HardwareConfig::FILTERS_PER_GROUP, c);
 
-                // For parallel cycle calculation
-                // int max_cycles = 0;
-                // std::vector<int> pe_cycles(Scnn::HardwareConfig::NUM_PE, 0);
-
-                /**************************************************************/
                 for (int pe_num = 0; pe_num < Scnn::HardwareConfig::NUM_PE; pe_num++) {
 
                     Scnn::Input_Buffer* input_tile = loader.IA_buffers[pe_num];
@@ -50,7 +45,6 @@ int main() {
                     while (!dispatcher.finished || dispatcher.output_valid || mult_array.has_output() || !buffer_queue.is_empty()) {
 
                         cycle++;
-                        // pe_cycles[pe_num]++;
 
                         accumulator.Cycle(&buffer_queue, &conv_layer.OA);
                         mult_array.Cycle(&dispatcher, &buffer_queue, &conv_layer.OA);
@@ -60,25 +54,7 @@ int main() {
                     total_idle_count += mult_array.idle_count;
                     total_count += mult_array.total_mults_count;
                     total_idle_cycle += mult_array.idle_cycle;
-
-                    // if (pe_cycles[pe_num] > max_cycles) {
-                    //     max_cycles = pe_cycles[pe_num];
-                    // }
                 }
-                /**************************************************************/
-
-                // calculate barrier cycles
-                // IDK!!!!!!!!!!!!
-
-                // for (int cyc : pe_cycles) {
-                //     int barrier_cycle = max_cycles - cyc;
-                //     cycle += barrier_cycle;
-                //     total_idle_cycle += barrier_cycle;
-                //     total_idle_count += barrier_cycle * Scnn::HardwareConfig::NUM_MULTIPLIERS;
-                //     total_count += barrier_cycle * Scnn::HardwareConfig::NUM_MULTIPLIERS;
-                // }
-
-
             }
         }
 
